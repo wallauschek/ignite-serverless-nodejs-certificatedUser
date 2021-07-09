4eb4b5ee1e501114697cf45515188516f675e04c
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-07-09 07:33:58 -0300

    serverless de criação de certificado

--- src/functions/hello.ts
+++
@@ -1,9 +0,0 @@
-export const handle = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({message: "Hello world Ignite Serverless"}),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-};
